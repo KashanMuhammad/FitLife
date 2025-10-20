7611c07ab21a500875453beb6bccb7fa7daec976
Fawad Ali Shan <fawadalishan010@gmail.com>
2025-10-20 20:36:04 +0530

    Reformated the lib directories and files

--- fitlife_app/lib/main.dart
+++ fitlife_app/lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:fitlife_app/Onboarding%20Screens/fitlife_splash_screen.dart';
+
 
 
 
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'home_screen.dart';
+import 'Screens/Onboarding Screens/fitlife_splash_screen.dart';
+
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
